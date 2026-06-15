4d8b5c468ac31892cd083535bbbd3b4bb13a2e86
unknown <voyan111@gmail.com>
2026-06-15 21:13:47 +0800

    admin seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -10,11 +10,13 @@ class AdminSeeder extends Seeder
 {
     public function run(): void
     {
-        User::create([
-            'name'     => 'Admin',
-            'email'    => 'admin@threadshop.com',
-            'password' => Hash::make('admin123'),
-            'role'     => 'admin',
-        ]);
+        User::firstOrCreate(
+            ['email' => 'admin@threadshop.com'],
+            [
+                'name'     => 'Admin',
+                'password' => Hash::make('admin123'),
+                'role'     => 'admin',
+            ]
+        );
     }
 }
